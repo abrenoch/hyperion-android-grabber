38be9ddd76f35e7b133b87e780c0ec48163bcb13
Dave <nlclothing@gmail.com>
2018-04-15 19:29:44 -0400

    some misc cleanups in mobile build

--- mobile/src/main/java/com/abrenoch/hyperiongrabber/SettingsActivity.java
+++ mobile/src/main/java/com/abrenoch/hyperiongrabber/SettingsActivity.java
@@ -1,25 +1,15 @@
 package com.abrenoch.hyperiongrabber;
 
 import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
 import android.view.MenuItem;
-
-import java.util.List;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
