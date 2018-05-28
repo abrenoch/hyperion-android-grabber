ed713de74d6fb96331dd27e6466a90adb506cc0c
Nino van Hooff <nino@highstreetapp.com>
2018-05-28 22:41:19 +0200

    network scanner: initial ui commit

---
+++ common/src/main/java/com/abrenoch/hyperiongrabber/common/activities/NetworkScanActivity.java
@@ -0,0 +1,15 @@
+package com.abrenoch.hyperiongrabber.common.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.abrenoch.hyperiongrabber.common.R;
+
+public class NetworkScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network_scan);
+    }
+}
