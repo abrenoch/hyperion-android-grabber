196bc46613928662e66c07bc7bae1a3a08f3c557
Nino van Hooff <ninovanhooff@gmail.com>
2018-06-03 12:02:55 +0200

    [TV] implement color bursts for ScanResultActivity

--- tv/src/main/java/com/abrenoch/hyperiongrabber/tv/activities/ScanResultActivity.java
+++ tv/src/main/java/com/abrenoch/hyperiongrabber/tv/activities/ScanResultActivity.java
@@ -12,12 +12,17 @@ import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.TextView;
 
+import com.abrenoch.hyperiongrabber.common.network.Hyperion;
 import com.abrenoch.hyperiongrabber.tv.R;
+
+import java.io.IOException;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import nl.dionsegijn.konfetti.KonfettiView;
 import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class ScanResultActivity extends LeanbackActivity {
     public static final String EXTRA_RESULT_HOST_NAME = "EXTRA_RESULT_HOST_NAME";
@@ -29,7 +34,12 @@ public class ScanResultActivity extends LeanbackActivity {
     @BindView(R.id.scanResultHostName) TextView hostNameText;
     @BindView(R.id.scanResultEmojiText) TextView emojiText;
     private String hostName;
-    private String port;
+    private int port;
+
+    private final int ORANGE = Color.rgb(253, 104, 40);
+    private final int[] COLORS = {Color.RED, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, ORANGE};
+    /** the amount of variation in R, G, B components of a confetti burst */
+    private final int BURST_COLOR_SPREAD = 200;
 
 
     @Override
@@ -43,7 +53,7 @@ public class ScanResultActivity extends LeanbackActivity {
         Bundle extras = getIntent().getExtras();
         if (extras != null){
             hostName = extras.getString(EXTRA_RESULT_HOST_NAME);
-            port = extras.getString(EXTRA_RESULT_PORT);
+            port = Integer.parseInt(extras.getString(EXTRA_RESULT_PORT));
             hostNameText.setText(hostName);
         } else {
             hostNameText.setText(R.string.error_no_host_name_extra);
@@ -72,19 +82,17 @@ public class ScanResultActivity extends LeanbackActivity {
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setHyperionColor(hostName, port, -1);
 
+    }
 
-    private void startKonfetti() {
-        konfettiView.build()
-                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
-                .setDirection(0.0, 359.0)
-                .setSpeed(1f, 5f)
-                .setFadeOutEnabled(true)
-                .setTimeToLive(400L)
-                .addShapes(Shape.RECT, Shape.CIRCLE)
-                .addSizes(new nl.dionsegijn.konfetti.models.Size(12, 5))
-                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
-                .streamFor(300, Long.MAX_VALUE);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setHyperionColor(hostName, port, Color.MAGENTA);
     }
 
     public void onClick(View v){
@@ -95,7 +103,10 @@ public class ScanResultActivity extends LeanbackActivity {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish(); // call this to finish the current activity
-            return;
+        } else if (v.getId() == R.id.changeColorButton){
+            int color = COLORS[new Random().nextInt(COLORS.length)];
+            setHyperionColor(hostName, port, color);
+            burstKonfetti(color);
         }
     }
 
@@ -104,7 +115,80 @@ public class ScanResultActivity extends LeanbackActivity {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString("hyperion_host", hostName);
-        editor.putString("hyperion_port", port);
+        editor.putString("hyperion_port", String.valueOf(port));
         editor.apply();
     }
+
+    private void startKonfetti() {
+        konfettiView.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(400L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(new Size(12, 5))
+                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
+                .streamFor(300, Long.MAX_VALUE);
+    }
+
+    /** Start a one-shot burst of confetti from a random location of a single color
+     *
+      * @param color a color int such as Color.RED, NOT a color resource
+     */
+    private void burstKonfetti(int color){
+        int r = Color.red(color);
+        int g = Color.green(color);
+        int b = Color.blue(color);
+
+        Random random = new Random();
+        int[] variations = new int[10];
+        int halfSpread = BURST_COLOR_SPREAD / 2;
+        for (int i = 0; i < variations.length; i++) {
+            variations[i] = Color.rgb(
+                    colorComponentClamp(r - halfSpread + random.nextInt(BURST_COLOR_SPREAD)),
+                    colorComponentClamp(g - halfSpread + random.nextInt(BURST_COLOR_SPREAD)),
+                    colorComponentClamp(b - halfSpread + random.nextInt(BURST_COLOR_SPREAD))
+            );
+        }
+
+        float posX = (float) (Math.random() * konfettiView.getWidth());
+        float posY = (float) (Math.random() * konfettiView.getHeight());
+
+        konfettiView.build()
+                .addColors(variations)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(400L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(new Size(12, 5))
+                .setPosition(posX, posY)
+                .burst(100);
+    }
+
+    /** A color int , or -1 to clear */
+    private void setHyperionColor(String hostName, int port, int color){
+        new Thread(() -> {
+            try {
+                Hyperion hyperion = new Hyperion(hostName, port);
+                if (hyperion.isConnected()){
+                    if (color == -1){
+                        hyperion.clear(50);
+                    } else {
+                        hyperion.setColor(color, 50);
+                    }
+                }
+                hyperion.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    /** [0 -255] */
+    private static int colorComponentClamp(int value){
+        return Math.min(255, Math.max(0, value));
+    }
+
 }
