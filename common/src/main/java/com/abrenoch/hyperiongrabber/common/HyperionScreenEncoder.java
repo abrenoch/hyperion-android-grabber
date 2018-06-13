1286b27546f3f577b9aca3749119975c4d6753e3
Dave <nlclothing@gmail.com>
2018-06-13 00:30:47 -0400

    border detection & removal

--- common/src/main/java/com/abrenoch/hyperiongrabber/common/HyperionScreenEncoder.java
+++ common/src/main/java/com/abrenoch/hyperiongrabber/common/HyperionScreenEncoder.java
@@ -125,7 +125,7 @@ public class HyperionScreenEncoder extends HyperionScreenEncoderBase {
                     long now = System.nanoTime();
                     Image img = reader.acquireLatestImage();
                     if (img != null && now - lastFrame >= min_nano_time) {
-                        mListener.sendFrame(savePixels(img), mWidthScaled, mHeightScaled);
+                        sendImage(img);
                         img.close();
                         lastFrame = now;
                     } else if (img != null) {
@@ -138,28 +138,50 @@ public class HyperionScreenEncoder extends HyperionScreenEncoderBase {
         }
     };
 
-    private byte[] savePixels(Image image){
-        Image.Plane plane = image.getPlanes()[0];
-        ByteBuffer buffer = plane.getBuffer();
+    private byte[] grabPixels(ByteBuffer buffer, int width, int height, int rowStride,
+        int pixelStride, int firstX, int firstY){
 
-        int width = image.getWidth();
-        int height = image.getHeight();
-        int pixelStride = plane.getPixelStride();
-        int rowPadding = plane.getRowStride() - width * pixelStride;
-
-        ByteArrayOutputStream bao = new ByteArrayOutputStream(width * height * 3);
-
+        int rowPadding = rowStride - width * pixelStride;
         int offset = 0;
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+
+        ByteArrayOutputStream bao = new ByteArrayOutputStream(
+                (width - firstX * 2) * (height - firstY * 2) * 3
+        );
+
+        for (int y = 0, compareHeight = height - firstY - 1; y < height; y++, offset += rowPadding) {
+            if (y < firstY || y > compareHeight) continue;
+            for (int x = 0, compareWidth = width - firstX - 1; x < width; x++, offset += pixelStride) {
+                if (x < firstX || x > compareWidth) continue;
                 bao.write(buffer.get(offset)); // R
                 bao.write(buffer.get(offset + 1)); // G
                 bao.write(buffer.get(offset + 2)); // B
-                offset += pixelStride;
             }
-            offset += rowPadding;
         }
 
         return bao.toByteArray();
+    }
+
+    private void sendImage(Image img) {
+        Image.Plane plane = img.getPlanes()[0];
+        ByteBuffer buffer = plane.getBuffer();
+
+        int width = img.getWidth();
+        int height = img.getHeight();
+        int pixelStride = plane.getPixelStride();
+        int rowStride = plane.getRowStride();
+        int firstX = 0;
+        int firstY = 0;
+
+        BorderObject border = findBorder(buffer, width, height, rowStride, pixelStride);
+        if (border.isKnown) {
+            firstX = border.horizontalBorderIndex;
+            firstY = border.verticalBorderIndex;
+        }
+
+        mListener.sendFrame(
+                grabPixels(buffer, width, height, rowStride, pixelStride, firstX, firstY),
+                width - firstX * 2,
+                height - firstY * 2
+        );
     }
 }
--- common/src/main/java/com/abrenoch/hyperiongrabber/common/HyperionScreenEncoderBase.java
+++ common/src/main/java/com/abrenoch/hyperiongrabber/common/HyperionScreenEncoderBase.java
@@ -13,13 +13,13 @@ public class HyperionScreenEncoderBase {
     private static final int TARGET_HEIGHT = 60;
     private static final int TARGET_WIDTH = 60;
     private static final int TARGET_BIT_RATE = TARGET_HEIGHT * TARGET_WIDTH * 3;
-    int mHeight;
-    int mWidth;
+    private int mHeight;
+    private int mWidth;
     int mWidthScaled;
     int mFrameRate;
     int mHeightScaled;
     int mDensity;
-    int mBlackThreshold = 5; // unit to detect how black a pixel is [0..255]
+    private int mBlackThreshold = 5; // unit to detect how black a pixel is [0..255] TODO: assign
 
     Handler mHandler;
 
@@ -71,20 +71,8 @@ public class HyperionScreenEncoderBase {
         return 1;
     }
 
-    public BorderObject findBorder(Image image) {
-        Image.Plane plane = image.getPlanes()[0];
-        ByteBuffer buffer = plane.getBuffer();
-        int width = image.getWidth();
-        int height = image.getHeight();
-        int rowStride = plane.getRowStride();
-        int pixelStride = plane.getPixelStride();
-        return this.findBorder(buffer, width, height, rowStride, pixelStride);
-    }
-
-    public BorderObject findBorder(ByteBuffer buffer, int width, int height, int rowStride,
-                                   int pixelStride) {
-
-//        int rowPadding = rowStride - width * pixelStride;
+    BorderObject findBorder(ByteBuffer buffer, int width, int height, int rowStride,
+                            int pixelStride) {
 
         int width33percent = width / 3;
         int width66percent = width33percent * 2;
@@ -102,25 +90,21 @@ public class HyperionScreenEncoderBase {
 
         buffer.position(0).mark();
 
-        int offset = 0;
         for (int x = 0, posYCLp; x < width33percent; x++) {
 
             // RGB values at 33% height - to left of image
             pos33p = (height33percent * rowStride) + (x * pixelStride);
-//            pos33p = offset + height33percent * rowStride;
             c1R = buffer.get(pos33p) & 0xff;
             c1G = buffer.get(pos33p + 1) & 0xff;
             c1B = buffer.get(pos33p + 2) & 0xff;
 
             // RGB values at 66% height - to left of image
             pos66p = (height66percent * rowStride) + (x * pixelStride);
-//            pos66p = offset + height66percent * rowStride;
             c2R = buffer.get(pos66p) & 0xff;
             c2G = buffer.get(pos66p + 1) & 0xff;
             c2B = buffer.get(pos66p + 2) & 0xff;
 
             // RGB values at center Y - to right of image
-//            posYCLp = (offset + yCenter * rowStride) + (rowStride - rowPadding - (pixelStride * 2) * (x + 1));
             posYCLp = (yCenter * rowStride) + ((width - x - 1) * pixelStride);
             c3R = buffer.get(posYCLp) & 0xff;
             c3G = buffer.get(posYCLp + 1) & 0xff;
@@ -131,31 +115,24 @@ public class HyperionScreenEncoderBase {
                 firstNonBlackXPixelIndex = x;
                 break;
             }
-
-            // increase the offset by the pixel stride
-//            offset += pixelStride;
         }
 
         buffer.reset();
-        offset = 0;
         for (int y = 0, posBCLp; y < height33percent; y++) {
 
             // RGB values at 33% width - top of image
-//            pos33p = offset + width33percent * pixelStride;
             pos33p = (width33percent * pixelStride) + (y * rowStride);
             c1R = buffer.get(pos33p) & 0xff;
             c1G = buffer.get(pos33p + 1) & 0xff;
             c1B = buffer.get(pos33p + 2) & 0xff;
 
             // RGB values at 66% width - top of image
-//            pos66p = offset + width66percent * pixelStride;
             pos66p = (width66percent * pixelStride) + (y * rowStride);
             c2R = buffer.get(pos66p) & 0xff;
             c2G = buffer.get(pos66p + 1) & 0xff;
             c2B = buffer.get(pos66p + 2) & 0xff;
 
             // RGB values at center X - bottom of image
-//            posBCLp = offset + ((height - y - 1) * (rowStride * 2)) + xCenter * pixelStride;
             posBCLp = (xCenter * pixelStride) + ((height - y - 1) * rowStride);
             c3R = buffer.get(posBCLp) & 0xff;
             c3G = buffer.get(posBCLp + 1) & 0xff;
@@ -166,9 +143,6 @@ public class HyperionScreenEncoderBase {
                 firstNonBlackYPixelIndex = y;
                 break;
             }
-
-            // increase the offset by the row stride
-//            offset += rowStride;
         }
 
         return new BorderObject(firstNonBlackXPixelIndex, firstNonBlackYPixelIndex);
@@ -179,10 +153,10 @@ public class HyperionScreenEncoderBase {
     }
 
     public class BorderObject {
-        public boolean isKnown;
-        public int horizontalBorderIndex;
-        public int verticalBorderIndex;
-        public BorderObject(int horizontalBorderIndex, int verticalBorderIndex) {
+        boolean isKnown;
+        int horizontalBorderIndex;
+        int verticalBorderIndex;
+        BorderObject(int horizontalBorderIndex, int verticalBorderIndex) {
             this.horizontalBorderIndex = horizontalBorderIndex;
             this.verticalBorderIndex = verticalBorderIndex;
             this.isKnown = this.horizontalBorderIndex != -1 && this.verticalBorderIndex != -1;
