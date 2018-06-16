eb23127d19177fb1a8a481e547e1a9240139b6a3
Nino van Hooff <ninovanhooff@gmail.com>
2018-06-16 22:38:46 +0200

    [TV] remove unused code from MasnualSetupActivity

--- tv/src/main/java/com/abrenoch/hyperiongrabber/tv/activities/ManualSetupActivity.java
+++ tv/src/main/java/com/abrenoch/hyperiongrabber/tv/activities/ManualSetupActivity.java
@@ -16,40 +16,17 @@
 
 package com.abrenoch.hyperiongrabber.tv.activities;
 
-import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v17.leanback.app.GuidedStepSupportFragment;
-import android.support.v17.leanback.widget.GuidanceStylist;
-import android.support.v17.leanback.widget.GuidanceStylist.Guidance;
-import android.support.v17.leanback.widget.GuidedAction;
 import android.support.v4.app.FragmentActivity;
 
-import com.abrenoch.hyperiongrabber.tv.R;
 import com.abrenoch.hyperiongrabber.tv.fragments.settings.BasicSettingsStepFragment;
 
-import java.util.List;
-
 /**
- * Activity that showcases different aspects of GuidedStepFragments.
+ * Allows to set all preferences anually
  */
 public class ManualSetupActivity extends FragmentActivity {
 
-
-    private static final int OPTION_CHECK_SET_ID = 10;
-    private static final String[] OPTION_NAMES = {
-            "Option A",
-            "Option B",
-            "Option C"
-    };
-    private static final String[] OPTION_DESCRIPTIONS = {
-            "Here's one thing you can do",
-            "Here's another thing you can do",
-            "Here's one more thing you can do"
-    };
-
-    private static final boolean[] OPTION_CHECKED = {true, false, false};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,65 +35,4 @@ public class ManualSetupActivity extends FragmentActivity {
             GuidedStepSupportFragment.addAsRoot(this, new BasicSettingsStepFragment(), android.R.id.content);
         }
     }
-
-    private static void addCheckedAction(List<GuidedAction> actions, Context context,
-                                         String title, String desc, boolean checked) {
-        GuidedAction guidedAction = new GuidedAction.Builder()
-                .title(title)
-                .description(desc)
-                .checkSetId(OPTION_CHECK_SET_ID)
-                .build();
-        guidedAction.setChecked(checked);
-        actions.add(guidedAction);
-    }
-
-    public static class ConfigurationTestStepFragment extends GuidedStepSupportFragment {
-
-        @Override
-        @NonNull
-        public Guidance onCreateGuidance(Bundle savedInstanceState) {
-            String title = getString(R.string.guidedstep_second_title);
-            String breadcrumb = getString(R.string.guidedstep_second_breadcrumb);
-            String description = getString(R.string.guidedstep_second_description);
-            Drawable icon = getActivity().getDrawable(R.mipmap.ic_launcher);
-            return new Guidance(title, description, breadcrumb, icon);
-        }
-
-        @Override
-        public GuidanceStylist onCreateGuidanceStylist() {
-            return new GuidanceStylist() {
-                @Override
-                public int onProvideLayoutId() {
-                    return R.layout.guidedstep_second_guidance;
-                }
-            };
-        }
-
-        @Override
-        public void onCreateActions(@NonNull List<GuidedAction> actions, Bundle savedInstanceState) {
-            String desc = getResources().getString(R.string.guidedstep_action_description);
-            actions.add(new GuidedAction.Builder(getContext())
-                    .title(getResources().getString(R.string.guidedstep_action_title))
-                    .description(desc)
-                    .multilineDescription(true)
-                    .infoOnly(true)
-                    .enabled(false)
-                    .build());
-            for (int i = 0; i < OPTION_NAMES.length; i++) {
-                addCheckedAction(actions,
-                        getActivity(),
-                        OPTION_NAMES[i],
-                        OPTION_DESCRIPTIONS[i],
-                        OPTION_CHECKED[i]);
-            }
-        }
-
-        @Override
-        public void onGuidedActionClicked(GuidedAction action) {
-            getActivity().finishAfterTransition();
-
-        }
-
-    }
-
 }
