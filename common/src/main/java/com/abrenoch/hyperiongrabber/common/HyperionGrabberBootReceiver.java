9dcc151b1c30b0648b7463ba388fe2870123cc93
Nino van Hooff <ninovanhooff@gmail.com>
2018-06-17 22:53:29 +0200

    use correct key in BootReceiver

--- common/src/main/java/com/abrenoch/hyperiongrabber/common/HyperionGrabberBootReceiver.java
+++ common/src/main/java/com/abrenoch/hyperiongrabber/common/HyperionGrabberBootReceiver.java
@@ -3,18 +3,18 @@ package com.abrenoch.hyperiongrabber.common;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
-import android.preference.PreferenceManager;
 import android.support.annotation.RequiresApi;
+
+import com.abrenoch.hyperiongrabber.common.util.Preferences;
 
 public class HyperionGrabberBootReceiver extends BroadcastReceiver {
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-            if (preferences.getBoolean("start_on_boot", false)) {
+            Preferences preferences = new Preferences(context);
+            if (preferences.getBoolean(R.string.pref_key_start_on_boot, false)) {
                 final Intent i = new Intent(context, BootActivity.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION
                         |Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
