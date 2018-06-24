09401a5a2834a81266c07d08bd183f3c57efcfc3
Dave <nlclothing@gmail.com>
2018-06-24 11:07:42 -0400

    added some debug logging (if configured)

--- common/src/main/java/com/abrenoch/hyperiongrabber/common/HyperionScreenEncoderBase.java
+++ common/src/main/java/com/abrenoch/hyperiongrabber/common/HyperionScreenEncoderBase.java
@@ -3,13 +3,15 @@ package com.abrenoch.hyperiongrabber.common;
 import android.media.projection.MediaProjection;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.abrenoch.hyperiongrabber.common.network.HyperionThread;
 
 public class HyperionScreenEncoderBase {
-    private static final String TAG = "HyperionScreenEncoderBase";
+    private static final boolean DEBUG = false;
+    private static final String TAG = "ScreenEncoderBase";
     private static final int TARGET_HEIGHT = 60;
     private static final int TARGET_WIDTH = 60;
     private static final int TARGET_BIT_RATE = TARGET_HEIGHT * TARGET_WIDTH * 3;
@@ -27,26 +29,51 @@ public class HyperionScreenEncoderBase {
     HyperionThread.HyperionThreadListener mListener;
 
     HyperionScreenEncoderBase(final HyperionThread.HyperionThreadListener listener,
-                             final MediaProjection projection, final int width, final int height,
-                             final int density, int frameRate) {
+                              final MediaProjection projection, final int width, final int height,
+                              final int density, int frameRate) {
+        if (DEBUG) Log.d(TAG, "Encoder starting");
+
         mListener = listener;
         mMediaProjection = projection;
         mDensity = density;
         mFrameRate = frameRate;
 
-        // enforce we have whole even numbers (though it seems unlikely we wouldn't already)
+        if (DEBUG) {
+            Log.d(TAG, "Density: " + String.valueOf(mDensity));
+            Log.d(TAG, "Frame Rate: " + String.valueOf(mFrameRate));
+            Log.d(TAG, "Original Width: " + String.valueOf(width));
+            Log.d(TAG, "Original Height: " + String.valueOf(height));
+            Log.d(TAG, "Target Bitrate: " + String.valueOf(TARGET_BIT_RATE));
+            Log.d(TAG, "Target Width: " + String.valueOf(TARGET_WIDTH));
+            Log.d(TAG, "Target Height: " + String.valueOf(TARGET_HEIGHT));
+        }
+
+        // enforce we have whole even numbers
         mWidth = (int) Math.floor(width);
         mHeight = (int) Math.floor(height);
         if (mWidth % 2 != 0) mWidth--;
         if (mHeight % 2 != 0) mHeight--;
 
+        if (DEBUG) {
+            Log.d(TAG, "Rounded Width: " + String.valueOf(mWidth));
+            Log.d(TAG, "Rounded Height: " + String.valueOf(mHeight));
+        }
+
         int divisor = findDivisor();
         mHeightScaled = (mHeight / divisor);
         mWidthScaled = (mWidth / divisor);
 
+        if (DEBUG) {
+            Log.d(TAG, "Common Divisor: " + String.valueOf(divisor));
+            Log.d(TAG, "Scaled Width: " + String.valueOf(mWidthScaled));
+            Log.d(TAG, "Scaled Height: " + String.valueOf(mHeightScaled));
+        }
+
         final HandlerThread thread = new HandlerThread(TAG);
         thread.start();
         mHandler = new Handler(thread.getLooper());
+
+        if (DEBUG) Log.d(TAG, "Encoder ready");
     }
 
     public boolean isCapturing() {
