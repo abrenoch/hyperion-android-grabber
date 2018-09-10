a9494b7104925e3007a1a14306f914c57fdee5f7
Dave <nlclothing@gmail.com>
2018-09-09 21:18:09 -0400

    added about activity

---
+++ mobile/src/main/java/com/abrenoch/hyperiongrabber/mobile/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.abrenoch.hyperiongrabber.mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
