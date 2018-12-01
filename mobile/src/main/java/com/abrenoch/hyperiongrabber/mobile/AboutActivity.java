a709df3d3900ef7934e4fc5dae9e4cd5f538078a
Dave <nlclothing@gmail.com>
2018-12-01 13:14:49 -0500

    updated about activity to show version & tweaked activity name/list title

--- mobile/src/main/java/com/abrenoch/hyperiongrabber/mobile/AboutActivity.java
+++ mobile/src/main/java/com/abrenoch/hyperiongrabber/mobile/AboutActivity.java
@@ -1,12 +1,14 @@
 package com.abrenoch.hyperiongrabber.mobile;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,9 +22,31 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        mResourcesLinearLayout = findViewById(R.id.resources_layout);
-        mDevelopersLinearLayout = findViewById(R.id.developers_layout);
-        mTranslatorsLinearLayout = findViewById(R.id.translators_layout);
+        setTitle(R.string.about_title);
+
+        Context context = getApplicationContext();
+        String versionName = null;
+        int versionCode = 0;
+
+        try {
+            PackageInfo pInfo = context.getPackageManager().getPackageInfo(getPackageName(), 0);
+            versionName = pInfo.versionName;
+            versionCode = pInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (versionCode != 0 && versionName != null) {
+            Resources res = getResources();
+            String versionText = String.format(res.getString(R.string.full_version_label), versionName,
+                    String.valueOf(versionCode));
+            TextView versionTextView = findViewById(R.id.appVersion);
+            versionTextView.setText(versionText);
+        }
+
+        mResourcesLinearLayout = findViewById(R.id.resourcesLayout);
+        mDevelopersLinearLayout = findViewById(R.id.developersLayout);
+        mTranslatorsLinearLayout = findViewById(R.id.translatorsLayout);
 
         fillValues(R.array.resource_links, mResourcesLinearLayout);
         fillValues(R.array.names_developers, mDevelopersLinearLayout);
